d1fd63badfd73cbe496d26bb23016c35d2291370
Abhinav Paste <valtrexcode01@gmail.com>
2026-03-01 18:46:17 +0530

    changes

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -8,8 +8,12 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func newdb() *pgxpool.Pool {
+func Newdb() *pgxpool.Pool {
 	dsn := os.Getenv("DATABASE_URL")
+
+	if dsn == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
 
 	pool, err := pgxpool.New(context.Background(), dsn)
 	if err != nil {
